db17038191d115bb171a63d55d95404770cf90e7
GitHub Action <action@github.com>
2021-03-28 15:20:52 +0800

    Update at 2021-03-28T07:20:51Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=271e7cb532818bfe.html
-ARGS=(--anonymous -m 210328-84429772-96ba-4277-941b-c1c1069883ea_a421677365 -e -n 0.001 --lang en)
+OUTPUT=e35cbc16475e86d4.html
+ARGS=(--anonymous -t 2021032815gm-00a9-0000-5aea4346 -a 2 --pt 90,45,0,-150 -n 0.05 --lang ja)
