feda9cacd13631bc701d80b08d6670afc736f7e1
GitHub Action <action@github.com>
2021-04-12 10:55:26 +0800

    Update at 2021-04-12T02:55:26Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=40d94dbdbc9e1f68.html
-ARGS=(--anonymous -m 210412-7d9bc222-4d50-4f38-97d5-7a3194b87705_a823758225 --pt 90,45,0,-135 -n 0.05 --lang ja)
+OUTPUT=24a6bb5d23e9d77d.html
+ARGS=(--anonymous -m 210412-647af2fd-1c6d-4a66-a84e-84c57a7b354b_a416371437 --pt 30,15,0,-180 -n 0.15 --lang ja)
