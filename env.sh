1fe1cc6a3e2e624b47b1460f24747fc17a74f570
GitHub Action <action@github.com>
2021-04-16 15:49:14 +0800

    Update at 2021-04-16T07:49:14Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=b2b23d3f0ebec1d7.html
-ARGS=(--anonymous -m 210416-dfad76d9-ff97-4c57-ba90-c48cff925c5e_a442934397 --pt 75,30,0,-135 -n 0.05 --lang ja)
+OUTPUT=d1c67e509b7c3d6b.html
+ARGS=(--anonymous -t 2021041616gm-00a9-0000-bdc2fff1 -a 2 --pt 90,45,0,-165 -n 0.05 --lang ja)
