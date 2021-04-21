723131494ea7f96dc99619e533c25658087ed75e
GitHub Action <action@github.com>
2021-04-21 22:11:03 +0800

    Update at 2021-04-21T14:11:02Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=b65d03d87b4fbadb.html
-ARGS=(--anonymous -m 210421-eca9a67f-50d3-4801-a9b0-3e12046b7004_a442934397 --pt 75,30,0,-135 -n 0.05 --lang ja)
+OUTPUT=a43e511dc881348c.html
+ARGS=(--anonymous -t 2021042116gm-0089-0000-efac0803 -a 2 --pt 60,15,0,-45 -n 0.05 --lang ja)
