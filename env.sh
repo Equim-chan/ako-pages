cfca716140f380b006cd41aa3844ac5653c81480
GitHub Action <action@github.com>
2021-04-28 12:48:21 +0800

    Update at 2021-04-28T04:48:20Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=11068c07ece8beaa.html
-ARGS=(--anonymous -t 2021042812gm-00a9-0000-1d2ea4e4 -a 1 --pt 90,45,0,-165 -n 0.05 --lang ja)
+OUTPUT=c4774486cd163996.html
+ARGS=(--anonymous -m jjjonu-ozt2x754-zab5-6ffi-f9oj-eefghtxuqrsw_a925291751_2 -e -n 0.001 --lang en)
