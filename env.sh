caa60c737382ce2ac1df1872f74200a6eabbf56c
GitHub Action <action@github.com>
2021-05-16 00:48:52 +0800

    Update at 2021-05-15T16:48:52Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=6edf99c71ac89018.html
-ARGS=(--anonymous -m 210516-d36551c7-8ae9-4127-b522-c41020733951_a458503688 --pt 90,45,0,-180 -n 0.05 --lang ja)
+OUTPUT=f1dcc52392c8f849.html
+ARGS=(--anonymous -t 2021051601gm-00a9-0000-edb09e74 -a 0 --pt 90,45,0,-150 -n 0.05 --lang ja)
