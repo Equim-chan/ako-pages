114b803fc815d086255a647a7971b376d88ecc72
GitHub Action <action@github.com>
2021-05-23 17:24:24 +0800

    Update at 2021-05-23T09:24:22Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=4b4854d576ff927e.html
-ARGS=(--anonymous -t 2021052317gm-0009-0000-7bc65158 -a 0 --pt 30,15,0,0 -n 0.05 --lang en)
+OUTPUT=ce8223334b383252.html
+ARGS=(--anonymous -t 2021052317gm-00a9-0000-b04ce606 -a 0 --pt 90,45,0,-135 -n 0.05 --lang ja)
