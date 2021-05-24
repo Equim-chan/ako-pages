ddedb965b99c90d0bf1d222524fe4a59223f9cb5
GitHub Action <action@github.com>
2021-05-24 23:43:04 +0800

    Update at 2021-05-24T15:43:04Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=0f72eb2132e1f7a5.html
-ARGS=(--anonymous -m 210524-a4900047-e0a6-4a60-b19f-e3632de2f99a_a879243334 --pt 90,45,0,-180 -n 0.05 --lang ja)
+OUTPUT=834d7fb809db56a0.html
+ARGS=(--anonymous -m 210524-3d3d758f-64d0-45f0-ab7e-365a33e20458_a87279435 -a 0 -e -n 0.001 --lang ja)
