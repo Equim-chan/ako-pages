cd2012a23082e23ffd9737e9cccec016e58f7e10
GitHub Action <action@github.com>
2021-06-05 03:15:58 +0800

    Update at 2021-06-04T19:15:57Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=9bf97c9fd33853be.html
-ARGS=(--anonymous -m 210605-774dd27c-6aa5-4a80-bc21-27c48e161ba4_a925035314 -e -n 0.001 --lang en)
+OUTPUT=3e37c86c8878e13d.html
+ARGS=(--anonymous -m 210605-d7359fe0-de2d-4d24-b6f0-5e6a1dc0247e_a436431022 --pt 60,30,0,-90 -n 0.05 --lang ja)
