dfed500abdc24fa6d5e827471c2ac7bfcee0fa2c
GitHub Action <action@github.com>
2021-06-07 04:32:49 +0800

    Update at 2021-06-06T20:32:49Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=e8e5658e5c8236ac.html
-ARGS=(--anonymous -m 210607-ce2e837a-180e-4390-8d8c-7213fbc971c9_a925189360 -e -n 0.001 --lang en)
+OUTPUT=da198681b2ce1aeb.html
+ARGS=(--anonymous -t 2021060705gm-00a9-0000-edea237d -a 2 --pt 90,45,0,-135 -n 0.05 --lang ja)
