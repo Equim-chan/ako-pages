ffdebab9793e6c988f869943e199bf467ca113a5
GitHub Action <action@github.com>
2021-06-23 01:41:20 +0800

    Update at 2021-06-22T17:41:20Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=dc1b5f7703f4dbcd.html
-ARGS=(--anonymous -t 2021062300gm-0029-0000-53c14d86 -a 0 -e -n 0.001 --lang ja)
+OUTPUT=55a15a0c7f1b1347.html
+ARGS=(--anonymous -t 2021062300gm-0029-0000-7bda905c -a 0 --pt 75,30,0,-90 -n 0.05 --lang en)
