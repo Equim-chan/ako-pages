cfdaf20f2a3916bfda8a17a8c3d550d5c080f861
GitHub Action <action@github.com>
2021-07-18 12:14:00 +0800

    Update at 2021-07-18T04:14:00Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=1c2d200b556ca043.html
-ARGS=(--anonymous -m 210718-f6bd6960-dd5d-4597-b622-2fa008f94a49_a435537527 -e -n 0.001 --lang ja)
+OUTPUT=7f32f618b15f78dc.html
+ARGS=(--anonymous -m 210718-f77485fb-2093-47eb-b635-39914758d390_a440098382 --pt 30,15,0,0 -n 0.05 --lang ja)
