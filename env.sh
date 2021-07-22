5ab4aed1f3d6dcd7d4760e2c0fb809c49016cc17
GitHub Action <action@github.com>
2021-07-22 17:50:23 +0800

    Update at 2021-07-22T09:50:23Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=cd0a97bffe7f6b45.html
-ARGS=(--anonymous -m 210719-f16d5535-4c51-4d20-ba9f-a08e107df8dd_a443380766 -e -n 0.001 --lang ja)
+OUTPUT=896ba0802d0650c3.html
+ARGS=(--anonymous -m 210615-240c345d-ff98-4021-94a1-ad6c794d5272_a423959615 --pt 75,30,0,-165 -n 0.05 -k '--kyokus e1' --lang ja)
