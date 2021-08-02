1c63047a2e1b99c64666abc803475cfc7df74f19
GitHub Action <action@github.com>
2021-08-02 15:52:03 +0800

    Update at 2021-08-02T07:52:01Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=8500b2ee5c87ee74.html
-ARGS=(--anonymous -m jjjslo-rtqr2460-4039-6bb8-f8kg-mgtfrioxywmv_a426301564_2 -e -n 0.001 --lang ja)
+OUTPUT=e480b8b012258201.html
+ARGS=(--anonymous -t 2021080216gm-00c1-0000-177781c3 -a 1 --pt 40,10,0,-50 -n 0.05 --lang ja)
