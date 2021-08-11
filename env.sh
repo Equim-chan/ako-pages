fee32a60ca69089b76d271edad889184868bd7dc
GitHub Action <action@github.com>
2021-08-11 20:17:34 +0800

    Update at 2021-08-11T12:17:34Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=fae559fc3333730c.html
-ARGS=(--anonymous -t 2021081120gm-0089-0000-923d6300 -a 1 --pt 60,15,0,-90 -n 0.05 --lang ja)
+OUTPUT=7a06f26686ad5f1f.html
+ARGS=(--anonymous -t 2021081120gm-0029-0000-126668a2 -a 3 -e -n 0.001 --lang ja)
