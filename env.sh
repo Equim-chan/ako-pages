dbcac5692cafc58e8602cf7c8ca80cc6d8dfc676
GitHub Action <action@github.com>
2021-09-07 08:16:42 +0800

    Update at 2021-09-07T00:16:42Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=356609caf619d84f.html
-ARGS=(--anonymous -m 210907-3d9a72c7-8ead-43f0-9488-ebcb52ada682_a15637972 -a 0 --pt 50,20,0,-90 -n 0.05 --lang ja)
+OUTPUT=c6f62919d286a820.html
+ARGS=(--anonymous -m 210906-8ca2ef74-4116-4f09-88ff-3132874c8339_a465952861 -e -n 0.001 --lang ja)
