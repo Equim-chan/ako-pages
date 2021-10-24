cb660e1014df37cbc6d0e730e38ad7a7d431927f
GitHub Action <action@github.com>
2021-10-24 21:07:01 +0800

    Update at 2021-10-24T13:07:01Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=1c2fddf413045ddf.html
-ARGS=(--anonymous -t 2021102417gm-00a9-0000-7906b3a5 -a 3 --pt 90,45,0,-165 -n 1 --lang ja)
+OUTPUT=c1ac55300b900246.html
+ARGS=(--anonymous -t 2021102417gm-00a9-0000-7906b3a5 -a 0 --pt 90,45,0,-135 -n 1 --lang ja)
