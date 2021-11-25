4ca6f91589c3d5c1924ba8a0680f11e7a45cdef9
GitHub Action <action@github.com>
2021-11-25 18:46:06 +0800

    Update at 2021-11-25T10:46:06Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=8f007cda8a4a151b.html
-ARGS=(--anonymous -t 2021112421gm-00a9-0000-7d85babc -a 3 --pt 90,45,0,-180 -n 0.05 --lang ja)
+OUTPUT=65669b02128af53c.html
+ARGS=(--anonymous -t 2021112519gm-00c1-0000-2c1cf0cf -a 0 --pt 40,10,0,-70 -n 0.05 --lang ja)
