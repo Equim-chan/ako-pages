7385642ad1f441e6471fb07892fa01b26e030fa4
GitHub Action <action@github.com>
2021-12-08 02:00:11 +0800

    Update at 2021-12-07T18:00:11Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=64479edf8fa70feb.html
-ARGS=(--anonymous -t 2021120716gm-0009-0000-096346b6 -a 1 -e -n 0.001 --lang ja)
+OUTPUT=f7c5cc4a1442ce85.html
+ARGS=(--anonymous -t 2021120802gm-0029-0000-be60aab9 -a 3 --pt 75,30,0,-120 -n 0.05 --lang ja)
