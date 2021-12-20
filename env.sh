c288b97d5002a5707180d7becdccd1a96bff5698
GitHub Action <action@github.com>
2021-12-20 23:47:17 +0800

    Update at 2021-12-20T15:47:17Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=23fab340dab2416c.html
-ARGS=(--anonymous -t 2021121918gm-00a9-0000-c25970b8 -a 2 --pt 90,45,0,-180 -n 0.05 --lang ja)
+OUTPUT=49d4346700155cab.html
+ARGS=(--anonymous -t 2021122100gm-0089-0000-ecee0c1c -a 1 --pt 60,15,0,-75 -n 0.05 --lang ja)
