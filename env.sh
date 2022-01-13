ccd224f0581d4adc0c7a60981458730456a00104
GitHub Action <action@github.com>
2022-01-13 15:34:11 +0800

    Update at 2022-01-13T07:34:11Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=f3b065e50af71bb7.html
-ARGS=(--anonymous -t 2022011314gm-0089-0000-92fc2cdb -a 3 --pt 60,15,0,-90 -n 0.05 --lang ja)
+OUTPUT=1d633979746e3874.html
+ARGS=(--anonymous -t 2022011315gm-00c1-0000-ce664f8b -a 3 -e -n 0.001 --lang ja)
