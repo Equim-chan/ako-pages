52923a12347f484725272e30a6ba6079b122fca1
GitHub Action <action@github.com>
2022-02-03 23:53:48 +0800

    Update at 2022-02-03T15:53:48Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=402c0f6ba85625f7.html
-ARGS=(--anonymous -t 2022020314gm-0029-0000-e96407e2 -a 3 --pt 75,30,0,-120 -n 0.5 --lang ja)
+OUTPUT=02d62209938db16e.html
+ARGS=(--anonymous -t 2022020400gm-0009-8145-7e31ba6b -a 3 -e -n 0.001 --lang ja)
