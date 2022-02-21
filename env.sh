56ae01b35aed81e3f6c2c42d3896fa2280161916
GitHub Action <action@github.com>
2022-02-21 22:34:05 +0800

    Update at 2022-02-21T14:34:05Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=759d9127eedf0a7a.html
-ARGS=(--anonymous -t 2021111900gm-00c1-0000-160c219d -a 2 --pt 60,15,0,-60 -n 0.05 --lang ja)
+OUTPUT=eb844bd6d7384308.html
+ARGS=(--anonymous -t 2022022122gm-00c1-0000-f7ca8d86 -a 2 --pt 40,10,0,-50 -n 0.05 --lang ja)
