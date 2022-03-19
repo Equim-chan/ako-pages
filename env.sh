b24cc2931c0005c3d9d7c9013d99180a3033286e
GitHub Action <action@github.com>
2022-03-19 19:46:47 +0800

    Update at 2022-03-19T11:46:47Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=9201098b17990557.html
-ARGS=(--anonymous -t 2022031918gm-0029-0000-f2f3d8b2 -a 0 --pt 90,45,0,-180 -n 0.05 --lang ja)
+OUTPUT=1e2a0f701318556c.html
+ARGS=(--anonymous -t 2022031920gm-0089-0000-d799e194 -a 1 -e -n 0.001 --lang ja)
