db3777424400a30d6cae2fc5a88fe09588c39332
GitHub Action <action@github.com>
2022-06-15 00:49:01 +0800

    Update at 2022-06-14T16:49:01Z

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=3dfb1bebde854608.html
-ARGS=(--anonymous -t 2022061500gm-0001-0000-dd933a6a -a 3 -e -n 0.001 --lang ja)
+OUTPUT=8379b6e0c2f88e90.html
+ARGS=(--anonymous -t 2022061501gm-00c1-0000-5f8f96fe -a 0 --pt 40,10,0,-50 -n 0.05 -k e4 --lang ja)
